1ae9d97d84dc0fed08c409a7a2b549d710ac1787
Saim Khan <saimkhank23v@gmail.com>
2026-01-07 15:04:27 +0500

    Add project files.

---
+++ Assignment-Api/Controllers/AssignmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_Api.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
