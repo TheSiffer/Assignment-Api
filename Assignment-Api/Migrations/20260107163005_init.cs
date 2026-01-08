2dbf65ae23dee8bb4a98dd250ea914406eb46251
Saim Khan <saimkhank23v@gmail.com>
2026-01-08 14:19:56 +0500

    Ef-core setup and migrations. Db connection added. ApplicationDb context added. Models added.

---
+++ Assignment-Api/Migrations/20260107163005_init.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assignment_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ICNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsBiometricEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsPolicyAccepted = table.Column<bool>(type: "bit", nullable: false),
+                    PolicyAcceptedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OtpRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsUsed = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OtpRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OtpRequests_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OtpRequests_UserId",
+                table: "OtpRequests",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_ICNumber",
+                table: "Users",
+                column: "ICNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OtpRequests");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Assignment-Api/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment_Api.Models
+{
+    public class Users
+    {
+        public int Id {  get; set; }
+        public string CustName { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(50)]
+        public string ICNumber { get; set; } = string.Empty;
+        public string PhoneNumber {  get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public bool IsBiometricEnabled { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public bool IsPolicyAccepted { get; set; }
+        public DateTime PolicyAcceptedAt { get; set; }
+        public ICollection<OtpRequest> Otp {  get; set; } = new List<OtpRequest>();
+    }
+}
